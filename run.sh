5921103f7ec2dd2483967bdf067f4adb7d2a2c9e
bhso <bhso@dblab.postech.ac.kr>
2019-09-04 17:06:49 +0900

    FIX example running script (run.sh)

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
-python3 ./web_crawler.py web_downloader sql sqls/web_downloader.sql;
-python3 ./web_crawler.py parser sql sqls/parser.sql;
-python3 ./web_crawler.py sql_extractor sql sqls/sql_extractor.sql;
-python3 ./web_crawler.py table_extractor sql sqls/table_extractor.sql;
-python3 ./web_crawler.py nl_extractor sql sqls/nl_extractor.sql
+python ./web_crawler.py web_downloader sql sqls/web_downloader.sql
+python ./web_crawler.py parser sql sqls/parser.sql
+python ./web_crawler.py sql_extractor sql sqls/sql_extractor.sql
+python ./web_crawler.py table_extractor sql sqls/table_extractor.sql
+python ./web_crawler.py nl_extractor sql sqls/nl_extractor.sql
